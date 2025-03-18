7d0234f54f9e6b9b8c99f944550732a6eb5b40e7
Eduardo Rojas <eduardo.rojas@wisebyte.com.br>
2025-03-17 22:43:42 -0300

    feat: atualizando projeto

--- src/components/Buttons/ButtonsIntroPage/index.jsx
+++ src/components/Buttons/ButtonsIntroPage/index.jsx
@@ -4,7 +4,9 @@ export default function ButtonsIntro ({onDownloadCv, targetModal = "#exampleModa
   return (
     <div className="about__buttons mt-4">
       <button data-bs-toggle="modal" data-bs-target={targetModal}>Sobre o Eduardo</button>
+      
       <button onClick={onDownloadCv}>Ver Currículo</button>
+      
       <a href="https://eduardorojas.com.br/contato/" target="_blank">
         <button data-bs-toggle="modal" data-bs-target={targetModal}>Contato</button>
       </a>
